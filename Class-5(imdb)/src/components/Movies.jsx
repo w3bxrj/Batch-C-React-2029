978dbb6638c6499eddeee6c0af64a76fd3e68638
mrinal1224 <iammrinalkc@gmail.com>
2026-02-27 11:58:15 +0530

    get data from tmdb

--- Class-5(imdb)/src/components/Movies.jsx
+++ Class-5(imdb)/src/components/Movies.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Banner from "./Banner";
 import MovieCard from "./MovieCard";
+import axios from "axios";
 
 function Movies() {
+  // Now get the movies data
+  useEffect(() => {
+    async function getMOvies() {
+      let data = await axios.get(
+        `https://api.themoviedb.org/3/movie/popular?api_key=3aec63790d50f3b9fc2efb4c15a8cf99&language=en-US&page=1`
+      );
+
+      console.log(data)
+    }
+
+    getMOvies();
+  }, []);
+
   return (
     <div>
       <Banner />
@@ -10,7 +24,6 @@ function Movies() {
         className=" mt-10 flex gap-4 flex-wrap justify-evenly 
       "
       >
-        <MovieCard />
         <MovieCard />
         <MovieCard />
         <MovieCard />
